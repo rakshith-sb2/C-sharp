4c675f7e548f59f77bd55bc69b280d23c800dc86
raghu prasad <prasadraghuks@gmail.com>
2022-09-12 13:20:42 +0530

    updated

---
+++ AssemblyDemo/AssemblyTest/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using CalculatorLib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Calculator calc = new Calculator();
+        int res = calc.add(100, 200);
+        Console.WriteLine(" Sum is " + res);
+        res = calc.multiply(10, 20);
+        Console.WriteLine(" Multiplication is " + res);
+
+
+
+
+
+
+    }
+}
---
+++ AssemblyDemo/CalculatorLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace CalculatorLib
+{
+    public class Calculator
+    {
+
+        public int add (int n1,int n2)
+        {
+            return n1 + n2;
+        }
+
+        public int multiply(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        public int subtract(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+    }
+}
