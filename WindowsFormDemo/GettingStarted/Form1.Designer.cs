541b66425c474c4a03f00c1ec02cf4f8777b9df5
raghu prasad <prasadraghuks@gmail.com>
2022-09-13 12:08:02 +0530

    updated

---
+++ WindowsFormDemo/GettingStarted/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace GettingStarted
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.TextBox();
+            this.btn = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(240, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Your Name";
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(409, 80);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(209, 26);
+            this.name.TabIndex = 1;
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(409, 170);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(218, 61);
+            this.btn.TabIndex = 2;
+            this.btn.Text = "Submit";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(337, 292);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 20);
+            this.result.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Blue;
+            this.Name = "Form1";
+            this.Text = "Windows Form - Getting Started";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Label result;
+    }
+}
+
