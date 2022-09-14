bcc3807005ee88fe2435a3b64a0aaba553ef1867
raghu prasad <prasadraghuks@gmail.com>
2022-09-14 13:36:05 +0530

    updated

---
+++ adodotnet/adonetprograms/adonetbasics/adonetbasics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data.SqlClient;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Connection();
+    }
+
+    static void Connection()
+    {
+        string cs = "Data Source=RAGHUPRASAD;Initial Catalog=flatworldsep2022;Integrated Security=true;";
+        SqlConnection con = new SqlConnection(cs);
+        try
+        {
+            con.Open();
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection has beeen successfully created");
+            }
+        }catch(SqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+}
