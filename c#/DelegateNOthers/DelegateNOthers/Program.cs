5fd830461b84c928c1424d5e2dc295659a188fb4
raghu prasad <prasadraghuks@gmail.com>
2022-09-16 12:52:13 +0530

    updated

--- c#/DelegateNOthers/DelegateNOthers/Program.cs
+++ c#/DelegateNOthers/DelegateNOthers/Program.cs
@@ -6,6 +6,7 @@ internal class Program
     {
         Console.WriteLine("Delegate Demo");
         //DelegateDemo.DemoDelegate();
-        AnonymousNLamda.AnonymousDemo();
+        // AnonymousNLamda.AnonymousDemo();
+        EventDemo.testEvent();
     }
 }
