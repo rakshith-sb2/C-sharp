5371534acac7d8c7a1cf6235899e2028ffdd06ee
raghu prasad <prasadraghuks@gmail.com>
2022-09-23 15:55:11 +0530

    added crud

---
+++ aspdotnet/CRUD/CRUD/StudentCRUDLatest.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD
+{
+    public partial class StudentCRUDLatest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void LinkButton1_Click(object sender,EventArgs e)
+        {
+            SqlDataSource1.InsertParameters["name"].DefaultValue =
+                ((TextBox)GridView1.FooterRow.FindControl("FooterName")).Text;
+            SqlDataSource1.InsertParameters["gender"].DefaultValue =
+                ((DropDownList)GridView1.FooterRow.FindControl("FooterDdlGender")).SelectedValue;
+            SqlDataSource1.InsertParameters["class"].DefaultValue =
+                ((TextBox)GridView1.FooterRow.FindControl("FooterClass")).Text;
+            int a = SqlDataSource1.Insert();
+            if (a > 0)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Insertion Successful')</script>");
+            }
+            else
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Insertion Failed')</script>");
+            }
+        }
+    }
+}
