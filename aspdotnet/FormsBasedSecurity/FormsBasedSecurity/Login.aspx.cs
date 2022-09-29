a8d113f164f227755a7c8a6cc5f2eb4dd1b95f89
raghu prasad <prasadraghuks@gmail.com>
2022-09-29 17:27:24 +0530

    updated

--- aspdotnet/FormsBasedSecurity/FormsBasedSecurity/Login.aspx.cs
+++ aspdotnet/FormsBasedSecurity/FormsBasedSecurity/Login.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Security;
@@ -19,8 +22,8 @@ namespace FormsBasedSecurity
         {
             // FormsAuthentication.
 
-
-            if (FormsAuthentication.Authenticate(txtUserName.Text, txtPassword.Text))
+            if (AuthenticateUser(txtUserName.Text, txtPassword.Text))
+            //  if (FormsAuthentication.Authenticate(txtUserName.Text, txtPassword.Text))
             {
                 //    FormsAuthentication.RedirectFromLoginPage(txtUserName.Text, false);
                       FormsAuthentication.RedirectFromLoginPage(txtUserName.Text, chkBoxRememberMe.Checked);
@@ -30,6 +33,31 @@ namespace FormsBasedSecurity
                 lblMessage.Text = "Invalid User Name and/or password";
             }
             
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            // ConfigurationManager class is in System.Configuration namespace
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            // SqlConnection is in System.Data.SqlClient namespace
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("spAuthenticateUser", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                // FormsAuthentication is in System.Web.Security
+                string EncryptedPassword = FormsAuthentication.HashPasswordForStoringInConfigFile(password, "SHA1");
+                // SqlParameter is in System.Data namespace
+                SqlParameter paramUsername = new SqlParameter("@UserName", username);
+                SqlParameter paramPassword = new SqlParameter("@Password", EncryptedPassword);
+
+                cmd.Parameters.Add(paramUsername);
+                cmd.Parameters.Add(paramPassword);
+
+                con.Open();
+                int ReturnCode = (int)cmd.ExecuteScalar();
+                return ReturnCode == 1;
+            }
         }
     }
 }
--- aspdotnet/SQLQuerykudvenkat.sql
+++ aspdotnet/SQLQuerykudvenkat.sql
@@ -38,3 +38,23 @@ END
 
 SELECT @ReturnCode AS ReturnValue
 END
+
+Create Procedure spAuthenticateUser
+@UserName nvarchar(100),
+@Password nvarchar(100)
+as
+Begin
+ Declare @Count int
+ 
+ Select @Count = COUNT(UserName) from tblUsers
+ where [UserName] = @UserName and [Password] = @Password
+ 
+ if(@Count = 1)
+ Begin
+  Select 1 as ReturnCode
+ End
+ Else
+ Begin
+  Select -1 as ReturnCode
+ End
+End
