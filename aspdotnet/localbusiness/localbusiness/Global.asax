44270662d73ac09aa0eee6e7fe9bc8dc8ecc7f90
raghu prasad <prasadraghuks@gmail.com>
2022-09-30 12:16:35 +0530

    updated

---
+++ aspdotnet/localbusiness/localbusiness/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="localbusiness.Global" Language="C#" %>
---
+++ aspdotnet/localbusiness/localbusiness/Site.Master.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace localbusiness
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void OnMenuItemDataBound(object sender, MenuEventArgs e)
+        {
+            if (SiteMap.CurrentNode != null)
+            {
+                if (e.Item.Text == SiteMap.CurrentNode.Title)
+                {
+                    if (e.Item.Parent != null)
+                    {
+                        e.Item.Parent.Selected = true;
+                    }
+                    else
+                    {
+                        e.Item.Selected = true;
+                    }
+                }
+            }
+        }
+    }
+}
