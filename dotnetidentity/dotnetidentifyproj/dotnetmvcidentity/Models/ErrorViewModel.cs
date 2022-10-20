713e0c49fd0aa4bb00af1074b2bd57a19cd11b31
raghu prasad <prasadraghuks@gmail.com>
2022-10-20 10:53:46 +0530

    updated

---
+++ dotnetidentity/dotnetidentifyproj/dotnetmvcidentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dotnetmvcidentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
