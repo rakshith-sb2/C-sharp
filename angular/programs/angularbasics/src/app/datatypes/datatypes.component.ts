b68f58fd9f9979eff608944518a2e6d5cc078b1d
raghu prasad <prasadraghuks@gmail.com>
2022-10-25 12:59:23 +0530

    datatypes

--- angular/programs/angularbasics/src/app/datatypes/datatypes.component.ts
+++ angular/programs/angularbasics/src/app/datatypes/datatypes.component.ts
@@ -6,10 +6,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datatypes.component.css']
 })
 export class DatatypesComponent implements OnInit {
+  /**
+  age = 10
+  age='ten';
+ */
+  /**
+   * Type script data types
+   * string,number,boolean
+   */
+  course:string='Angular'
+  duration:number = 20
+  isRegistered:boolean=true
+  credit:number = 3.5
+  courses:string[]=['HTML','CSS','Java Script','C#']
+  student={
+    id:1,
+    name:'ravi',
+    location:'Bengaluru'
+  }
 
-  constructor() { }
+  students=[
+
+    {
+      id:1,
+      name:'ravi',
+      location:'Bengaluru'
+    },
+    {
+      id:1,
+      name:'ravi',
+      location:'Bengaluru'
+    }
+  ]
+  // any is like  var in javascript
+  anytype:any='any thing can be assignment'
+
+  undefinedtype:undefined;
+
+  constructor() {
+    console.log('DatatypesComponent:constructor')
+   }
 
   ngOnInit(): void {
+    console.log('DatatypesComponent:ngOnInit')
+  }
+
+  greet():string{
+    return 'Deepavali Greetings'
   }
 
 }
