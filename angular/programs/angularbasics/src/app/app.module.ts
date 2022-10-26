896c1e7a60b1f48bf7906c5d26ec1b910f40f7c0
raghu prasad <prasadraghuks@gmail.com>
2022-10-26 16:52:39 +0530

    databinding

--- angular/programs/angularbasics/src/app/app.module.ts
+++ angular/programs/angularbasics/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { DatatypesComponent } from './datatypes/datatypes.component';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { DatabindingComponent } from './components/databinding/databinding.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatatypesComponent,
     EmployeeComponent,
-    CustomerComponent
+    CustomerComponent,
+    DatabindingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
