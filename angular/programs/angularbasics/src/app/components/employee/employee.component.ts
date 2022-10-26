1fb54898fd5ae2662bfcf7b6f0d63f2e4f81443b
raghu prasad <prasadraghuks@gmail.com>
2022-10-26 14:54:55 +0530

    structuraldirectives

---
+++ angular/programs/angularbasics/src/app/components/employee/employee.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  employees:any=[]
+  displaysal:boolean=false
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.employees=[
+      {
+        code:1,
+        name:'Ranga',
+        dept:'IT',
+        desg:'SE',
+        sal:25000
+      },
+      {
+        code:2,
+        name:'Smitha',
+        dept:'HR',
+        desg:'Executive',
+        sal:20000
+      },
+      {
+        code:3,
+        name:'Rajesh',
+        dept:'Admin',
+        desg:'Senior Executive',
+        sal:30000
+      },
+      {
+        code:4,
+        name:'Smith',
+        dept:'IT',
+        desg:'Manager',
+        sal:75000
+      }
+  
+  ]
+  }
+
+}
