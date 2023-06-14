67d0b302858eb978e18a0b5a119b285bc9c3f619
rakshit.sb <rakshit.sb@FWS-LP-1373>
2023-06-14 11:32:40 +0530

    test23

--- webapi/webapi/1-WebAPI/Controllers/WeatherForecastController.cs
+++ webapi/webapi/1-WebAPI/Controllers/WeatherForecastController.cs
@@ -18,49 +18,49 @@ namespace _1_WebAPI.Controllers
             _logger = logger;
         }
 
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var message = "Hi, welcome! Good evening!";
+        [HttpGet(Name = "GetWeatherForecastss")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        [HttpGet(Name = "GetWeatherForecastssss")]
+        public IEnumerable<WeatherForecast> Gets()
+        {
+            var message = "Hi, welcome! Good evening!";
 
-        //    var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-        //        Message = message // Set the message for each forecast
-        //    })
-        //    .ToArray();
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Message = message // Set the message for each forecast
+            })
+            .ToArray();
 
-        //    return weatherForecasts;
-        //}
-        //[HttpGet(Name = "GetWeatherForecast")]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var message = "Hi, welcome! Good evening!";
+            return weatherForecasts;
+        }
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Getss()
+        {
+            var message = "Hi, welcome! Good evening!";
 
-        //    var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = Random.Shared.Next(-20, 55),
-        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-        //        Message = message // Set the Message property to the desired greeting
-        //    })
-        //    .ToArray();
-            
-        //    return weatherForecasts;
-        //}
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+                Message = message // Set the Message property to the desired greeting
+            })
+            .ToArray();
+
+            return weatherForecasts;
+        }
 
     }
 }
