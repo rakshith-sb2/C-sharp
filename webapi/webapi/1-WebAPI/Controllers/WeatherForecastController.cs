e97b5be1e2bd80d92e8b4250194d11089ced983f
rakshit.sb <rakshit.sb@FWS-LP-1373>
2023-06-14 10:30:10 +0530

    test2

--- webapi/webapi/1-WebAPI/Controllers/WeatherForecastController.cs
+++ webapi/webapi/1-WebAPI/Controllers/WeatherForecastController.cs
@@ -32,17 +32,21 @@ namespace _1_WebAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            string greeting = "Hi, welcome! Good evening!";
+            var message = "Hi, welcome! Good evening!";
 
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var weatherForecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                Message = greeting
+                Message = message // Set the message for each forecast
             })
             .ToArray();
+
+            return weatherForecasts;
         }
-    
-}
+
+
+
+    }
 }
--- webapi/webapi/1-WebAPI/WeatherForecast.cs
+++ webapi/webapi/1-WebAPI/WeatherForecast.cs
@@ -3,12 +3,11 @@ namespace _1_WebAPI
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
+        public string Summary { get; set; }
+        public string Message { get; set; } // New property for the message
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-        public string Message { get; set; }
     }
+
 }
